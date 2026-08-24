3d8992da13b0e858e10b56d621aae0d31d2fa6fd
Harsh0604-2006 <harshmanoj0604@gmail.com>
2026-08-24 12:01:01 +0530

    splash of code

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Dashboard from './MyComponents/Dashboard.jsx'
+import DiseaseDetection from './MyComponents/DiseaseDetection.jsx';
+
+function App() {
+  return <DiseaseDetection />
+}
+
+export default App
